8015d49b4e3df399083adeb60529e73f76d0ce38
Aayush-Bhargav <122374889+Aayush-Bhargav@users.noreply.github.com>
2024-07-25 18:13:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import NoteState from './context/notes/NoteState.js';
 import Alert from './components/Alert.jsx';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Login from './components/Login.js';
 import Signup from './components/Signup.js';
 import NoteContext from './context/notes/noteContext.js'
@@ -17,12 +18,12 @@ function App() {
   return (
     <NoteState>
       <Router>
-        <Navbar title="My Notebook" />
+        <NavbarWrapper/>
         <div style={{ height: "50px" }}>
           <AlertWrapper />
         </div>
 
-        <div className='container' style={{ minHeight: "80vh" }}>
+        <div className='container' style={{ minHeight: "83vh" }}>
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
@@ -38,5 +39,14 @@ function App() {
 const AlertWrapper = () => {
   const { alert, showAlert } = useContext(NoteContext);
   return showAlert ? <Alert alert={alert} /> : null;
+};
+const NavbarWrapper = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;//only if current location is this , then set login prop to false so it shows the logout button
+  if (currentPath === '/login' || currentPath === '/signup')
+    return <Navbar loggedIn={false} title="My Notebook"/>;
+  else 
+    return <Navbar loggedIn={true} title="My Notebook"/>;
+  
 };
 export default App;
